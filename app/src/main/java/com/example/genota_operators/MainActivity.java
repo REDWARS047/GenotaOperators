9bb7c08fb1565614c506f82c7939e2f489d10541
REDWARS047 <redwars047@gmail.com>
2021-02-28 17:44:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/genota_operators/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.genota_operators;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView display;
+        display = findViewById(R.id.displaytxt);
+
+        int birthYear = 2002;
+        int birthMonth = 3;
+        int birthday = 12;
+
+        int currentYear = 2021;
+        int currentMonth = 1;
+        int currentDay = 26;
+
+        int ageInYears,ageInMonths,ageInDays;
+
+        if(birthMonth > currentMonth){
+            currentMonth += 12;
+            currentYear -= 1;
+        }
+
+        if(birthday > currentDay){
+            currentDay += 30;
+            currentMonth -= 1;
+        }
+
+        ageInYears = currentYear - birthYear;
+        ageInMonths = currentMonth - birthMonth;
+        ageInDays = currentDay - birthday;
+
+        display.setText("My precise age is\n"+
+                String.valueOf(ageInYears)+
+                " Years,\n"+
+                String.valueOf(ageInMonths)+
+                " Months,\n"+
+                String.valueOf(ageInDays)+
+                " Days.");
+    }
+}
